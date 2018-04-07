aca503519d46fcaec371ec8bcfa51e25fe5076a4
jw255 <jw255@st-andrews.ac.uk>
2018-04-07 13:31:13 +0100

    method stubs

---
+++ stupid.api.py
@@ -0,0 +1,20 @@
+def newProject(name):
+    pass
+
+def newFunction(name, args):
+    pass
+
+def newCondition(if_, then_, else_):
+    pass
+
+def newLoop(breakCond, loopN):
+    pass
+
+def fillLoop(statements):
+    pass
+
+def takeInput():
+    pass
+
+def produceOutput():
+    pass
